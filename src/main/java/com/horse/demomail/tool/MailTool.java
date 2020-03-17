30ab3ce9eda5f0e2e5769266d3e895ce20f5830b
Ma <903563099@qq.com>
2020-03-17 13:23:31 +0800

    Update MailTool.java
    remove password

--- src/main/java/com/horse/demomail/tool/MailTool.java
+++ src/main/java/com/horse/demomail/tool/MailTool.java
@@ -19,7 +19,7 @@ public class MailTool {
             // 发送人的邮箱
             email.setFrom("rootxmg@163.com", "root");
             // 如果需要认证信息的话，设置认证：用户名-密码。分别为发件人在邮件服务器上的注册名称和密码
-            email.setAuthentication("rootxmg@163.com", "rootqwe520");
+            email.setAuthentication("rootxmg@163.com", "root");
             // 要发送的邮件主题
             email.setSubject("这是一个java验证码demo");
             // 要发送的信息，可以使用了HtmlEmail，实现邮件内容中使用HTML标签
